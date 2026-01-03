8cf56332cd9ca08e6333f0ed0e3a5b68d77ceb9d
Samplats79 <samplatsasubonteng124@gmail.com>
2026-01-03 01:35:14 +0100

    feat: setup router with home and login routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Login from "../views/Login.vue";
 
 const routes = [
-  { path: "/", redirect: "/login" },
-  { path: "/login", component: Login },
   {
-    path: "/configurator",
-    component: () => import("../views/Configurator.vue"),
+    path: "/",
+    name: "home",
+    component: () => import("../views/Home.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/Login.vue"),
+  },
+  // placeholder (maken we later echt)
+  {
+    path: "/community",
+    name: "community",
+    component: () => import("../views/Community.vue"),
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
-
-router.beforeEach((to) => {
-  const token = localStorage.getItem("token");
-  if (to.path === "/configurator" && !token) return "/login";
-  if (to.path === "/login" && token) return "/configurator";
-  return true;
 });
 
 export default router;
